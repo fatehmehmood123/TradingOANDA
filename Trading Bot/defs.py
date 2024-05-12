c3634c998ceed24069fc4bbb88566cfe1f5e28b5
Fateh Mehmood <fatehmehmood123@gmail.com>
2024-05-12 10:26:29 +0500

    First Commit

---
+++ Trading Bot/defs.py
@@ -0,0 +1,16 @@
+API_KEY = "2276caa3176f9b11895554d0b231efb9-bc63b4ea67436d82e051698f4ac90c43"
+ACCOUNT_ID = "101-001-29056085-001"
+OANDA_URL = 'https://api-fxpractice.oanda.com/v3'
+
+SECURE_HEADER = {
+    'Authorization': f'Bearer {API_KEY}',
+    'Content-Type': 'application/json'
+}
+
+
+BUY = 1
+SELL = -1
+NONE = 0
+
+
+
---
+++ defs.py
@@ -0,0 +1,7 @@
+API_KEY = "38fbf83044b5a124bb9c8e24edc491dc-4230b4e450ba7ccf4cad67337c56ec18"
+ACCOUNT_ID = "101-001-28983516-001"
+OANDA_URL = 'https://api-fxpractice.oanda.com/v3'
+
+SECURE_HEADER = {
+    'Authorization': f'Bearer {API_KEY}'
+}
